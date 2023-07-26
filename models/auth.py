1fd655e12b04d4021c12f5d26dde34fb8a91dbef
JesusDBurgos <113454776+JesusDBurgos@users.noreply.github.com>
2023-07-26 16:27:25 -0500

    Update auth.py

--- models/auth.py
+++ models/auth.py
@@ -4,5 +4,5 @@ from models.base import Base
 class User(Base):
     __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    username = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(255))
