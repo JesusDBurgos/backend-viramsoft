ceb6a0dba76f4b8a821841426cea8bff5702b2bb
JesusDBurgos <113454776+JesusDBurgos@users.noreply.github.com>
2023-08-15 00:19:31 -0500

    Update detallePedModel.py

--- models/detallePedModel.py
+++ models/detallePedModel.py
@@ -5,7 +5,7 @@ from models.base import Base
 class DetallePedido_table(Base):
     __tablename__ = "detallePedido"
     idDetalle = Column( Integer,primary_key=True,nullable=False)
-    idPedido = Column(Integer, ForeignKey("pedido.idPedido"), nullable=False )
-    idProducto = Column( Integer, ForeignKey("producto.idProducto"),primary_key=True, nullable=False)
+    idPedido = Column(Integer, ForeignKey("pedido.idPedido"),primary_key=True, nullable=False )
+    idProducto = Column( Integer, ForeignKey("producto.idProducto"), nullable=False)
     cantidad = Column( Integer, nullable=False)
     precio = Column(Float, nullable=False)
