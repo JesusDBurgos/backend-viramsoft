c222a1800d376d83d23fd96d7f91d177a5b9d210
JesusDBurgos <113454776+JesusDBurgos@users.noreply.github.com>
2023-09-11 09:37:29 -0500

    Actualización de endpint crear pedido ASYNC
    Se modifica función para ser asincrona

--- config/database.py
+++ config/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.index import Base
+from databases import Database
 
-engine = create_engine("mysql+pymysql://pruebas@34.31.54.110/viramsoft")
+DATABASE_URL = "mysql+pymysql://pruebas@34.31.54.110/viramsoft"
+engine = create_engine(DATABASE_URL)
 
 conn = engine.connect()
+
+database = Database(DATABASE_URL)
 
 SessionLocal = sessionmaker(engine)
 
