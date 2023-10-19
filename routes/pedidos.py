9ef81f2049907a7b975a8b78d1cc4bdb929b2993
JesusDBurgos <113454776+JesusDBurgos@users.noreply.github.com>
2023-10-19 11:29:23 -0500

    cambiosxD

--- routes/pedidos.py
+++ routes/pedidos.py
@@ -3,6 +3,7 @@ from config.database import conn,get_db
 from models.index import Pedido_table,DetallePedido_table, Producto_table, Cliente_table
 from schemas.index import PedidoAggPydantic,ProductoPydantic, ProductosPedAggPydantic 
 from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import select,update, text
 from datetime import datetime
 from itertools import count
