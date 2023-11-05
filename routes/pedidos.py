4f8814e94c6d3450211602e8c966a5d8df1405b4
JesusDBurgos <113454776+JesusDBurgos@users.noreply.github.com>
2023-11-05 18:06:23 -0500

    cambio en respuesta de endpoint pedidos

--- routes/pedidos.py
+++ routes/pedidos.py
@@ -29,13 +29,15 @@ def get_orders(db: Session = Depends(get_db)):
     if not orders:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontraron pedidos")
     
+    vendedor_nombre = db.query(User).filter(User.id == vendedor["id"]).first()
+
     # Crear una lista para almacenar los pedidos formateados
     formatted_orders = []
     # Recorrer la lista de pedidos y construir el diccionario de respuesta
     for order in orders:
         order_dict = {
             "documentoCliente": order.clientes.documento,  # Obtener el documento del cliente
-            "vendedor": order.idVendedor,
+            "vendedor": vendedor_nombre.nombre,
             "observacion": order.observacion,
             "fechaEntrega": str(order.fechaEntrega),
             "estado": order.estado,
